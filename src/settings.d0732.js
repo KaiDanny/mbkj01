5e97d2bfb45524bee889e7ebaaec5e4099e14f80
danny <121385.cool@163.com>
2021-12-30 21:10:00 +0800

    test

---
+++ src/settings.d0732.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/load.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"1f567",main:"f473e"}};
