076984649db3fd1d42c2187a87a2414294e0fb3e
KaiDanny <381498086@qq.com>
2023-01-08 10:12:21 +0800

    Add files via upload
    water sort

---
+++ src/settings.8e842.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/launch.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"fcd15",main:"f0db5"}};
